1f4ccc3ca75966a9cee6eb4a5e2f5b5cb607ac24
riveco0120 <richardsk.vellojin@gmail.com>
2022-06-05 12:43:31 -0500

    Consumiendo api pokemon
    Se logro consumir la api para traer la foto del pokemon consultado por su nombre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
 import { BodyComponent } from './body/body/body.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pokemon/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon/pokemon.component.ts
@@ -8,12 +8,15 @@ import { CharacterService } from 'src/app/characters.servic';
 export class PokemonComponent {
 
     nombrePokemon:string ="";
+    urlImg:string="";
     
     constructor(private characterService:CharacterService){
 
     }
 
+    //Consumiendo la api con una subcribe 
     search(){
-      this.characterService.getPokemon(this.nombrePokemon)
+      this.characterService.getPokemon(this.nombrePokemon).subscribe((pokemon:any)=>
+        this.urlImg = pokemon.sprites.front_default);
     }
 }
