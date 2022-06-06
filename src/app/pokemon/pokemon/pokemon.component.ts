6158211ecf61caebff251b13b99d3a02cd962a12
riveco0120 <richardsk.vellojin@gmail.com>
2022-06-06 09:56:51 -0500

    Agregando estadisticas basicas

--- src/app/pokemon/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon/pokemon.component.ts
@@ -9,6 +9,12 @@ export class PokemonComponent {
 
     nombrePokemon:string ="";
     urlImg:string="";
+    nombreReal:string=""
+    id:number=0;
+    experiencia:number=0;
+    localizacion:string=""
+    orde:number=0;
+    tamano:number=0;
     
     constructor(private characterService:CharacterService){
 
@@ -21,9 +27,17 @@ export class PokemonComponent {
         this.nombrePokemon="";
        }
        else {
-      this.characterService.getPokemon(this.nombrePokemon).subscribe((pokemon:any)=>
-        this.urlImg = pokemon.sprites.front_default);
-       }
+      this.characterService.getPokemon(this.nombrePokemon).subscribe((pokemon:any)=>{
+        this.urlImg = pokemon.sprites.front_default;
+        this.id=pokemon.id;
+        this.experiencia=pokemon.base_experience;
+        this.localizacion=pokemon.location_area_encounters;
+        this.orde=pokemon.order;
+        this.tamano=pokemon.weight;
+        this.nombreReal=pokemon.name
+      console.log(pokemon)});
+      
+      }
     }
 
     
